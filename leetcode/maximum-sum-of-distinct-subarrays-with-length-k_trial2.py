eb5d97121c92aac3eb4ba7e86e23651bea27d0a7
Goodluck Alphaxard <masterprogrammerg@gmail.com>
2026-03-20 22:15:11 +0300

    Add solution for Maximum Sum of Distinct Subarrays With Length K (trial 2)

---
+++ leetcode/maximum-sum-of-distinct-subarrays-with-length-k_trial2.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maximumSubarraySum(self, nums: List[int], k: int) -> int:
+        freq = defaultdict(int)
+        left = 0
+        curr_sum = 0
+        max_sum = 0
+
+        for right in range(len(nums)):
+            freq[nums[right]] +=1
+            curr_sum += nums[right]
+
+            while freq[nums[right]]>1 or (right-left +1) >k:
+                freq[nums[left]] -= 1
+                curr_sum -= nums[left]
+
+                if freq[nums[left]] == 0:
+                    del freq[nums[left]]
+
+                left +=1
+            if (right - left +1) ==k:
+                max_sum = max(max_sum,curr_sum)
+        return max_sum
